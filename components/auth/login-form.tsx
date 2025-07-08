3eb9bbbf04f2955561b814ee2b673648587abd4e
mohd shakoor <samsuddin.s924@gmail.com>
2025-07-08 15:18:03 +0530

    setup:

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,15 @@
+import { CardWrapper } from "@/components/auth/card-wrapper"
+
+export const LoginForm = () =>{
+
+    return(
+       <CardWrapper
+       headerLabel="welcome back"
+  backButtonLabel = "Don't have an account?"
+  backButtonHref = "/auth/register"
+  showSocial 
+       >
+        Login Form!
+       </CardWrapper>
+    )
+}
