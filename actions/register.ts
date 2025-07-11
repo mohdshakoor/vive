e5d0a0ce7db7f720ea8df68be05ccdba383acbd8
mohd shakoor <samsuddin.s924@gmail.com>
2025-07-11 23:39:32 +0530

    create-user

--- actions/register.ts
+++ actions/register.ts
@@ -2,7 +2,10 @@
 
 import { RegisterSchema} from "@/schemas";
 
+import { db } from "@/lib/db";
+import bcrypt from "bcrypt";
 import * as z from "zod";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values : z.infer<typeof RegisterSchema>) => {
    const validateFields = RegisterSchema.safeParse(values);
@@ -10,6 +13,25 @@ export const register = async (values : z.infer<typeof RegisterSchema>) => {
    if(!validateFields.success){
     return { error: "Invalid fields!"};
    }
-   return {success:"Email sent!"};
     
+   const {email,password,name} = validateFields.data;
+   const hashedpassword = await bcrypt.hash(password,10)
+    
+   const existingUser = await getUserByEmail(email);                  
+
+   if(existingUser){
+      return {error: "Email already in use!"}
+   }
+
+   await db.user.create({
+    data:{
+      name,
+      email,
+      password:hashedpassword,
+    },
+   });
+
+   // TODO: Send varification token
+
+   return {success:"User created"};
 };
