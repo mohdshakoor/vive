067d468c6700ffd7a808046c6a3ccd298889f324
mohd shakoor <samsuddin.s924@gmail.com>
2025-07-18 10:56:01 +0530

    middlware

--- actions/login.ts
+++ actions/login.ts
@@ -1,8 +1,11 @@
 "use server";
 
-import { LoginSchema } from "@/schemas";
-
 import * as z from "zod";
+import { AuthError } from "next-auth";
+
+import { LoginSchema } from "@/schemas";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const login = async (values : z.infer<typeof LoginSchema>) => {
    const validateFields = LoginSchema.safeParse(values);
@@ -10,6 +13,23 @@ export const login = async (values : z.infer<typeof LoginSchema>) => {
    if(!validateFields.success){
     return { error: "Invalid fields!"};
    }
-   return {success:"Email sent!"};
     
+   const { email, password} = validateFields.data;
+    try {
+      await signIn("credentials", {
+         email,
+         password,
+         redirectTo:  DEFAULT_LOGIN_REDIRECT,
+      })
+    } catch (error) {
+        if(error instanceof AuthError) {
+         switch (error.type) {
+            case "CredentialsSignin":
+               return {error: "invalid credentials!"}
+            default: 
+            return { error: "something went wrong!"}
+         }
+        }
+        throw error;
+    }
 };
